463cafdce23fa31048662e90563f04c2a8199a28
ArijitNandi101 <arijitn001@gmail.com>
2021-01-18 11:02:29 +0530

    added dev environment vaiables to .env file.

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+require("dotenv").config();
 const express = require("express");
+const { MongoClient } = require("mongodb");
 const app = express();
-const server = app.listen(3030);
+const server = app.listen(
+    process.env.PORT,
+    () => console.log(`server strted on port ${process.env.PORT}`)
+);
