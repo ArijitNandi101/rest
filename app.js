5ebeb637096f5cfa83576665c3fc614e7f8c99e4
ArijitNandi101 <arijitn001@gmail.com>
2021-01-18 11:22:14 +0530

    created initial client connection to MongoDB URI

--- app.js
+++ app.js
@@ -2,7 +2,32 @@ require("dotenv").config();
 const express = require("express");
 const { MongoClient } = require("mongodb");
 const app = express();
+
+app.use(express.json());
+
 const server = app.listen(
     process.env.PORT,
     () => console.log(`server strted on port ${process.env.PORT}`)
 );
+
+const client = new MongoClient(
+    process.env.DATABASE_URI,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+);
+client.connect(async function(err) {
+    console.log("trying connection");
+    if(err){
+        console.error(err.stack);
+        client.close();
+        server.close();
+        console.log("connection failed")
+        return;
+    }
+    console.log("client connected");
+});
+
+process.on('SIGINT', (code) => {
+    client.close();
+    console.log(`Database Connection closed. Exiting with code: ${code}`);
+    process.exit();
+});
