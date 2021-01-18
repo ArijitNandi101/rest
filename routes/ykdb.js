8f23084ddba0f7e07e67b845992c28505b8a11bc
ArijitNandi101 <arijitn001@gmail.com>
2021-01-18 16:29:33 +0530

    extended middleware chain to verify request body content type. create mongodb aggregating pipeline to retrieve required output fields.

--- routes/ykdb.js
+++ routes/ykdb.js
@@ -1,13 +1,22 @@
 const router = require("express").Router();
 
-postMiddleware = [
-    function collectionExists(req,res,next) {
-        res.setHeader("Content-Type", "application/json");
+postMiddlewares = [
+    function contentTypeValidation(req,res,next){
         res.locals.result = {
             code: null,
             msg: null,
             records: null
+        };
+        if(req.is('application/json')){
+            res.setHeader("Content-Type", "application/json");
+            next();
+        } else {
+            res.locals.result.code = 1;
+            res.locals.result.msg = `Content-Type is wrong.`
+            res.status(400).send(res.locals.result);
         }
+    },
+    function collectionExists(req,res,next) {
         let collectionExists = false;
         for(let collectionName of req.app.locals.ykdbCollectionNamesList){
             if(collectionName === req.params.collection){
@@ -17,7 +26,7 @@ postMiddleware = [
         }
         if(collectionExists) next();
         else {
-            res.locals.result.code = 10;
+            res.locals.result.code = 2;
             res.locals.result.msg = `requested collection not found.`
             res.status(400).send(res.locals.result);
         };
@@ -30,38 +39,70 @@ postMiddleware = [
             req.body.minCount === undefined || 
             req.body.maxCount === undefined
         ){
-            res.locals.result.code = 1;
+            res.locals.result.code = 3;
             res.locals.result.msg = "invalid request payload";
             res.status(400).send(res.locals.result);
         } else next();
     },
     function(req,res,next) {
         res.locals.startDate = new Date(
-            req.body.startDate.replace(/(\d{4})-(\d{2})-(\d{2})/,"$2/$3/$1")
+            req.body.startDate
         );
         res.locals.endDate = new Date(
-            req.body.endDate.replace(/(\d{4})-(\d{2})-(\d{2})/,"$2/$3/$1")
+            req.body.endDate
         );
-        if(isNaN(res.locals.startDate) || isNaN(res.locals.endDate)){
-            res.locals.result.code = 2;
-            res.locals.result.msg = "Invalid date format";
+        res.locals.minCount = Number(req.body.minCount);
+        res.locals.maxCount = Number(req.body.maxCount);
+        if(
+            isNaN(res.locals.startDate) ||
+            isNaN(res.locals.endDate) ||
+            isNaN(res.locals.minCount) ||
+            isNaN(res.locals.maxCount)
+        ){
+            res.locals.result.code = 4;
+            res.locals.result.msg = "Invalid format";
             res.status(400).send(res.locals.result);
         } else next();
     }
 ];
 
-router.get("/",async function(req,res){
-    const db = req.app.locals.ykdb;
-    const result = req.app.locals.ykdbCollectionNamesList;
-    res.send(result);
-});
-
-router.post("/:collection",postMiddleware,async function(req,res){
+router.post("/:collection",postMiddlewares,async function(req,res){
     try{
         let ykdb = req.app.locals.ykdb;
-        const cursor = await ykdb.collection(
+        let pipeline = [
+            { 
+                $match : {
+                    createdAt: { 
+                    $gte: res.locals.startDate, 
+                    $lte: res.locals.endDate }
+                }
+            },
+            {
+                $project: {
+                    _id: 0,
+                    key: 1,
+                    createdAt: 1,
+                    totalCount: { 
+                        $cond: { 
+                            if: { $isArray: "$counts" }, 
+                            then: { $size: "$counts" }, 
+                            else: "NA"
+                        } 
+                    }
+                }   
+            },
+            { 
+                $match : { 
+                    totalCount: { 
+                        $gte: res.locals.minCount, 
+                        $lte: res.locals.maxCount 
+                    } 
+                } 
+            }
+        ];
+        let cursor = await ykdb.collection(
             req.params.collection
-        ).find({}).limit(100);
+        ).aggregate(pipeline);
         const data = await cursor.toArray();
         res.locals.result.code = 0;
         res.locals.result.msg = "success";
@@ -69,7 +110,7 @@ router.post("/:collection",postMiddleware,async function(req,res){
         res.status(200).send(res.locals.result);
     } catch(err){
         console.log(err.stack);
-        res.status(400).send("bad request.");
+        res.status(400).send({code:11,msg:"bad request.",records:null});
     }
 });
 
